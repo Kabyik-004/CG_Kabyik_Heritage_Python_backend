e3c3c0b7a38f39ea37e50ed1d7a63d714b268ea3
Kabyik-004 <kabyikpaul9@gmail.com>
2026-06-10 12:20:26 +0530

    Add shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,1 +1,14 @@
-git 
+amount = float(input("Enter the total shopping amount (₹): "))
+
+if amount > 5000:
+    discount = amount * 0.20
+elif amount > 2000:
+    discount = amount * 0.10
+else:
+    discount = 0
+
+final_bill = amount - discount
+
+print(f"\nOriginal Amount: ₹{amount:,.2f}")
+print(f"Discount Applied: ₹{discount:,.2f}")
+print(f"Final Payable Amount: ₹{final_bill:,.2f}")
